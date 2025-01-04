1c5f590a711138e0ef80d02c88433d04a083e665
Joel Valerio <jev66@cornell.edu>
2025-01-04 01:33:19 -0500

    Comments for how to implement Auth

--- wishlist/services/user-service/main.go
+++ wishlist/services/user-service/main.go
@@ -8,19 +8,21 @@ import (
 
 	"github.com/Saparta/wishlist/wishlist/services/user-service/endpoints"
 	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/joho/godotenv"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
+
+// var googleAuthConfig = &oauth2.Config{}
 
 func createUsersTable(dbpool *pgxpool.Pool) error {
 	query := `
 	CREATE TABLE IF NOT EXISTS users (
 		id UUID PRIMARY KEY,
-		oauthid UUID NOT NULL,
+		oauth_id UUID NOT NULL,
 		email TEXT NOT NULL,
 		name TEXT NOT NULL,
-		createdat TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+		created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 	);
 	`
 
@@ -33,7 +35,6 @@ func createUsersTable(dbpool *pgxpool.Pool) error {
 }
 
 func setUpDb(channel chan *pgxpool.Pool) {
-	godotenv.Load()
 	var dsn string = os.Getenv("DSN")
 
 	dbPool, err := pgxpool.New(context.Background(), dsn)
@@ -48,6 +49,76 @@ func setUpDb(channel chan *pgxpool.Pool) {
 	channel <- dbPool
 }
 
+// func GetUserInfo(accessToken string) (map[string]interface{}, error) {
+// 	userInfoEndpoint := "https://www.googleapis.com/oauth2/v2/userinfo"
+// 	resp, err := http.Get(fmt.Sprintf("%s?access_token=%s", userInfoEndpoint, accessToken))
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	defer resp.Body.Close()
+
+// 	var userInfo map[string]interface{}
+// 	if err := json.NewDecoder(resp.Body).Decode(&userInfo); err != nil {
+// 		return nil, err
+// 	}
+
+// 	return userInfo, nil
+// }
+
+// func SignJWT(userInfo map[string]interface{}) (string, error) {
+// 	// Customize the claims as needed
+// 	claims := jwt.MapClaims{
+// 		"sub":   userInfo["id"],
+// 		"name":  userInfo["name"],
+// 		"email": userInfo["email"],
+// 		"iss":   "oauth-app-golang",
+// 		"exp":   time.Now().Add(time.Hour * 24 * 5).Unix(), // 5 days
+// 	}
+
+// 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+// 	signedToken, err := token.SignedString([]byte("your-secret-key")) // Replace with your actual secret key
+// 	if err != nil {
+// 		return "", err
+// 	}
+
+// 	return signedToken, nil
+// }
+
+// func googleCallbackHandler(ctx *gin.Context) {
+// 	code := ctx.Query("code")
+// 	token, err := googleAuthConfig.Exchange(context.Background(), code)
+// 	if err != nil {
+// 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+// 		return
+// 	}
+// 	userInfo, err := GetUserInfo(token.AccessToken)
+// 	if err != nil {
+// 		fmt.Println("Error getting user info: " + err.Error())
+// 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+// 		return
+// 	}
+
+// 	signedToken, err := SignJWT(userInfo)
+// 	if err != nil {
+// 		fmt.Println("Error signing token: " + err.Error())
+// 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+// 		return
+// 	}
+
+// 	ctx.JSON(http.StatusOK, gin.H{"token": signedToken})
+// }
+
+// func init() {
+// 	godotenv.Load()
+// 	googleAuthConfig = &oauth2.Config{
+// 		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
+// 		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
+// 		RedirectURL:  os.Getenv("GOOGLE_REDIRECT_URL"), // Should be a frontend url
+// 		Scopes:       []string{"profile", "email"},
+// 		Endpoint:     google.Endpoint,
+// 	}
+// }
+
 func main() {
 	var dbChannel chan *pgxpool.Pool = make(chan *pgxpool.Pool)
 	go setUpDb(dbChannel)
@@ -58,10 +129,13 @@ func main() {
 			"message": "pong",
 		})
 	})
+
 	var dbPool *pgxpool.Pool = <-dbChannel
 	defer dbPool.Close()
 
+	r.Use(cors.Default())
 	r.GET("/users", func(ctx *gin.Context) { endpoints.GetUsers(ctx, dbPool) })
+	// r.GET("/auth/google/callback", googleCallbackHandler)
 
 	r.Run()
 }
