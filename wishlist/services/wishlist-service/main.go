807734ee8cf523be584f93925bc872db174cf897
Joel Valerio <jev66@cornell.edu>
2025-01-04 21:09:46 -0500

    Fixed SQL queries

--- wishlist/services/wishlist-service/main.go
+++ wishlist/services/wishlist-service/main.go
@@ -33,30 +33,34 @@ func createTables(dbpool *pgxpool.Pool) error {
 	query := `
 	CREATE TABLE IF NOT EXISTS wishlists(
 		id UUID PRIMARY KEY,
-		FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
+		user_id UUID,
 		title VARCHAR(255),
 		description VARCHAR(255),
 		is_public boolean,
 		created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 		last_modified TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-		last_opened TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+		last_opened TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+		FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
 	);
 	CREATE TABLE IF NOT EXISTS items(
 		id UUID PRIMARY KEY,
-		FOREIGN KEY(wishlist_id) REFERENCES wishlists(id) ON DELETE CASCADE,
+		wishlist_id UUID,
 		name VARCHAR(255),
 		url TEXT,
 		price REAL,
 		is_gifted boolean,
 		FOREIGN KEY(gifted_by) REFERENCES users(id) ON DELETE SET NULL,
-		created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+		created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+		FOREIGN KEY(wishlist_id) REFERENCES wishlists(id) ON DELETE CASCADE
 	);
 	CREATE TABLE IF NOT EXISTS shared(
 		id UUID PRIMARY KEY,
-		FOREIGN KEY(wishlist_id) REFERENCES wishlists(id) ON DELETE CASCADE,
-		FOREIGN KEY(shared_with) REFERENCES users(id) ON DELETE CASCADE,
+		wishlist_id UUID,
+		shared_with UUID,
 		can_edit boolean,
 		created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+		FOREIGN KEY(wishlist_id) REFERENCES wishlists(id) ON DELETE CASCADE,
+		FOREIGN KEY(shared_with) REFERENCES users(id) ON DELETE CASCADE
 	);
 	`
 	_, err := dbpool.Exec(context.Background(), query)
