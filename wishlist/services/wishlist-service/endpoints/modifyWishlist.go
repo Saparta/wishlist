5104232e116f615059922a4b262d3e7ba305942e
Joel Valerio <jev66@cornell.edu>
2025-01-08 21:58:23 -0500

    Fixed bug in modifyWishlist RPC

--- wishlist/services/wishlist-service/endpoints/modifyWishlist.go
+++ wishlist/services/wishlist-service/endpoints/modifyWishlist.go
@@ -2,6 +2,7 @@ package endpoints
 
 import (
 	"context"
+	"errors"
 	"time"
 
 	pb "github.com/Saparta/wishlist/wishlist/services/wishlist-service/proto"
@@ -19,23 +20,31 @@ func (W *WishlistService) ModifyWishlist(ctx context.Context, request *pb.Modify
 		return nil, status.Error(codes.Internal, "Failed to retrieve database connection from context")
 	}
 	rows, err := dbPool.Query(ctx, `
+	WITH authorized_users AS (
+    SELECT w.id AS wishlist_id
+    FROM wishlists w
+    LEFT JOIN shared s ON w.id = s.wishlist_id
+    WHERE w.id = $1
+      AND (w.user_id = $2 OR (s.shared_with = $2 AND s.can_edit = TRUE))
+	)
 	UPDATE wishlists SET
-		title = $1,
-		description = $2,
-		is_public = $3
+		title = $3,
+		description = $4,
+		is_public = $5
 	WHERE 
-		id = $4 AND user_id = $5
-	RETURNING id, user_id, title, description, is_public, can_edit, created_at, last_modified, last_opened;`, request.Title, request.Description, request.Id, request.IsPublic, request.UserId)
+		id in (SELECT wishlist_id FROM authorized_users)
+	RETURNING id, user_id, title, description, is_public, created_at, last_modified, last_opened;`, request.Id, request.UserId, request.Title, request.Description, request.IsPublic)
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, "Failed to query database: %s", err.Error())
 	}
 	result, err := pgx.CollectOneRow(rows, func(row pgx.CollectableRow) (*pb.ModifyWishlistResponse, error) {
 		var result pb.ModifyWishlistResponse
 		var createdAt, lastModified, lastOpened time.Time
-		err = row.Scan(&result.Id, &result.UserId, &result.Title, &result.Description, &result.IsPublic, &result.CanEdit, &createdAt, &lastModified, &lastOpened)
+		err = row.Scan(&result.Id, &result.UserId, &result.Title, &result.Description, &result.IsPublic, &createdAt, &lastModified, &lastOpened)
 		if err != nil {
 			return nil, err
 		}
+		result.CanEdit = func() *bool { b := true; return &b }()
 		result.CreatedAt = timestamppb.New(createdAt)
 		result.LastModified = timestamppb.New(lastModified)
 		result.LastOpened = timestamppb.New(lastOpened)
@@ -43,6 +52,9 @@ func (W *WishlistService) ModifyWishlist(ctx context.Context, request *pb.Modify
 	})
 
 	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, status.Errorf(codes.InvalidArgument, "User isn't authorized")
+		}
 		return nil, status.Errorf(codes.Internal, "Parsing results failed: %s", err.Error())
 	}
 
